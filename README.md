e21e8842cab5610aa48b933e8cb821c579938c50
dymafr <erwan1309@hotmail.com>
2022-04-01 14:33:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-tjywc1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-tjywc1)
---
+++ src/PageB.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="card p-20 d-flex justify-content-center align-items-center">
+    <h1>La page B</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
